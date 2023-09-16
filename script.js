d7579f412a4dfcd2931507a3cf033e140f38530b
Michael Chukwu <imchukwu@gmail.com>
2023-09-16 23:22:08 +0100

    fixed div alignment

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// const imgDataBtn = document.querySelector(".bottom-svg");
-const btnTest = document.querySelector(".btnTest");
-const idElement = document.querySelector(".slip_id");
-const output = document.querySelector(".centered-p");
-const svgElement = document.querySelector(".bottom-svg");
-const alternateImage = document.querySelector(".alternate-svg");
+const adviceNumberElement = document.querySelector(".advice-id");
+const adviceMessageElement = document.querySelector(".advice-message");
+const adviceDiceBtnElement = document.querySelector(".advice-dice");
 
-// // Add a click event listener to the SVG element
-// svgElement.addEventListener("click", () => {
-//     // Your code to handle the click event goes here
-//     alert("SVG clicked!"); // Example: Show an alert
-// });
-
-svgElement.addEventListener("click", () => {
+adviceDiceBtnElement.addEventListener("click", () => {
   const apiUrl = "https://api.adviceslip.com/advice";
 
   fetch(apiUrl)
@@ -22,23 +13,10 @@ svgElement.addEventListener("click", () => {
       return response.json();
     })
     .then((data) => {
-      idElement.textContent = JSON.stringify(data.slip.id);
-      output.textContent = JSON.stringify(data.slip.advice);
+      adviceNumberElement.textContent = JSON.stringify(data.slip.id);
+      adviceMessageElement.textContent = JSON.stringify(data.slip.advice);
     })
     .catch((error) => {
-      output.textContent = `Error: ${error.message}`;
+        adviceMessageElement.textContent = `Error: ${error.message}`;
     });
 });
-
-
-// // Add a mouseover event listener to swap the images
-// svgElement.addEventListener("mouseover", () => {
-//     alternateImage.classList.remove('hidden');
-//     svgElement.classList.add('hidden');
-// });
-
-// // Add a mouseout event listener to revert to the default image
-// alternateImage.addEventListener("mouseout", () => {
-//     svgElement.classList.remove('hidden');
-//     alternateImage.classList.add('hidden');
-// });
